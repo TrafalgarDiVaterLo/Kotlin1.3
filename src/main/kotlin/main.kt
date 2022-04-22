28c60367b37383e293ee7d135df2729f2bb7dc23
Trafalgar Lo <Sanshez249704@bk.ru>
2022-04-22 20:30:34 +0900

    add transaction

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,42 +1,35 @@
 fun main() {
-    println(agoToText(20))
-    println(agoToText(61))
-    println(agoToText(5000))
-    println(agoToText(95000))
-    println(agoToText(190000))
-    println(agoToText(400000))
+    println("Комиссия ${comissionCalc(card = "Visa", transactionHistory = 150000, transactionSum = 150000)}")
+    println("Комиссия ${comissionCalc(card = "Mastercard", transactionHistory = 3500, transactionSum = 3500)}")
+    println("Комиссия ${comissionCalc(card = "Mastercard", transactionHistory = 7500001, transactionSum = 7500000)}")
+    println("Комиссия ${comissionCalc(card = "Mastercard", transactionHistory = 7500, transactionSum = 7500)}")
 }
 
-fun agoToText(secondsAgo: Int): String {
-    return when (secondsAgo) {
-        in 0..60 -> "Был(а) только что"
-        in 61..60 * 60 -> "${secondToMinutes(secondsAgo)} назад"
-        in 60 * 60 + 1..24 * 60 * 60 -> "${secondToHour(secondsAgo)} назад"
-        in 24 * 60 * 60 + 1..24 * 60 * 60 * 2 -> "Был(а) сегодня"
-        in 24 * 60 * 60 * 2 + 1..24 * 60 * 60 * 3 -> "Был(а) вчера"
-        else -> "Был(а) давно"
+fun comissionCalc(card: String = "Vk pay", transactionHistory: Int = 0, transactionSum: Int) : Int? {
+    if(inLimits(card, transactionHistory, transactionSum)) {
+        return when (card) {
+            "Mastercard", "Maestro" -> return if ((transactionHistory < 7500000) && (transactionSum < 7500000)) {
+                0
+            } else {
+                ((transactionSum * 0.006 + 2000) / 100).toInt()
+            }
+            "Visa", "Мир" -> when {
+                transactionSum * 0.0075 > 3500 -> transactionSum * 0.0075.toInt()
+                else -> 3500
+            } else -> 0
+        }
+    } else {
+        return null
     }
 }
 
-fun secondToMinutes(secondsAgo: Int): String {
-    val minutesAgo = secondsAgo / 60
-    var secondToMinutes = ""
-    when {
-        minutesAgo in 2..4 || (minutesAgo % 10) in 2..4 -> secondToMinutes = "$minutesAgo минуты"
-        minutesAgo == 1 || ((minutesAgo % 10) == 1 && minutesAgo != 11 && minutesAgo %100 != 11) -> secondToMinutes = "$minutesAgo минуту"
-        else -> secondToMinutes = "$minutesAgo минут"
-
+fun inLimits(card: String, transactionHistory: Int, transactionSum: Int): Boolean {
+    return when {
+        card == "Vk Pay" -> return when {
+            transactionHistory < 4000000 && transactionSum < 150000 -> true
+            else -> false
+        }
+        transactionSum < 15000000 && transactionHistory < 60000000 -> true
+        else -> false
     }
-    return secondToMinutes
-}
-
-fun secondToHour(secondsAgo: Int): String {
-    val hourAgo = secondsAgo / 60 / 60
-    var secondToHour = ""
-    when {
-        hourAgo in 2..4 || (hourAgo % 10) in 2..4 -> secondToHour = "$hourAgo часа"
-        hourAgo == 1 || ((hourAgo % 10) == 1 && hourAgo != 11 && hourAgo %100 != 11) -> secondToHour = "$hourAgo час"
-        else -> secondToHour = "$hourAgo часов"
-    }
-    return secondToHour
 }
